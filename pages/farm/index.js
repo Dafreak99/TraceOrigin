22745b73e758b40f8ec27019d4bf7868827d8366
hai <sl4mdunk1999@gmail.com>
2021-04-13 12:59:27 +0700

    adding farm authentication link

--- pages/farm/index.js
+++ pages/farm/index.js
@@ -19,6 +19,7 @@ import Layout from "@/components/dashboard/Layout";
 import fetcher from "@/utils/fetcher";
 import { AiFillEdit } from "react-icons/ai";
 import EnterpriseAuthenticationModal from "@/components/dashboard/EntepriseAuthenticationModal";
+import Link from "next/link";
 
 const Info = () => {
   const [isEdit, setIsEdit] = useState(false);
@@ -132,6 +133,16 @@ const enterpriseAuthentication = (
       <Alert status="success" mt="2rem" w="max-content">
         <AlertIcon />
         Đã chứng thực doanh nghiệp
+        <Link href="/farm/authentication">
+          <a
+            style={{
+              marginLeft: "5px",
+              textDecoration: "underline",
+            }}
+          >
+            Xem chứng thực.
+          </a>
+        </Link>
       </Alert>
     );
   }
