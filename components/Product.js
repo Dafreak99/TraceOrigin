33d3e108b7238c6ae4fd23b70d7d06a9ca2e5861
hai <sl4mdunk1999@gmail.com>
2021-05-29 15:17:49 +0700

    change links

--- components/Product.js
+++ components/Product.js
@@ -2,6 +2,7 @@ import { Text, Flex, Box } from "@chakra-ui/react";
 import { AiOutlineQrcode } from "react-icons/ai";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Product = ({ images, name, _id }) => {
   const router = useRouter();
@@ -12,31 +13,35 @@ const Product = ({ images, name, _id }) => {
       direction="column"
       gridColumn={{ base: "span 6", xl: "span 3" }}
     >
-      <Box
-        height="300px"
-        width="100%"
-        position="relative"
-        onClick={() => router.push(`/product/${_id}`)}
-      >
-        <Image
-          src={images[0]}
-          objectFit="cover"
-          layout="fill"
-          style={{ borderRadius: "5px" }}
-        />
-      </Box>
+      <Link href={`/product/${_id}`}>
+        <a>
+          <Box
+            height="300px"
+            width="100%"
+            position="relative"
+            // onClick={() => router.push(`/product/${_id}`)}
+          >
+            <Image
+              src={images[0]}
+              objectFit="cover"
+              layout="fill"
+              style={{ borderRadius: "5px" }}
+            />
+          </Box>
 
-      <Flex align="center" mt={8}>
-        <Box
-          as={AiOutlineQrcode}
-          height="40px"
-          width="40px"
-          marginRight="30px"
-        />
-        <Text color="gray.600" fontSize="md">
-          {name}
-        </Text>
-      </Flex>
+          <Flex align="center" mt={8}>
+            <Box
+              as={AiOutlineQrcode}
+              height="40px"
+              width="40px"
+              marginRight="30px"
+            />
+            <Text color="gray.600" fontSize="md">
+              {name}
+            </Text>
+          </Flex>
+        </a>
+      </Link>
     </Flex>
   );
 };
